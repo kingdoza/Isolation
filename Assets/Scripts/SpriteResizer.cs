8e6f663a77a6a1ab5652f81c2ae0d07eb647676a
kingdoza <kdowon2k98@naver.com>
2025-07-05 13:50:46 +0900

    feat : postprocessing, objects

--- Assets/Scripts/SpriteResizer.cs
+++ Assets/Scripts/SpriteResizer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteResizer : MonoBehaviour
 {
-    private const float TargetWidth = 1920;
+    private const float TargetWidth = 1800;
 
 
 
