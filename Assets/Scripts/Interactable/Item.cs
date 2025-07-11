fa4bf175620bf1e8ca36053fc598ecce22cd5e88
alexcvbn <30707801+alexcvbn@users.noreply.github.com>
2025-07-12 06:30:35 +0900

    Revert "--"
    This reverts commit 59e0f0ed19f560664b7a2d098af193a51a2c3a0d.

--- Assets/Scripts/Interactable/Item.cs
+++ Assets/Scripts/Interactable/Item.cs
@@ -4,22 +4,13 @@ using UnityEngine.EventSystems;
 public class Item : MonoBehaviour, IInteractable, IPointerClickHandler
 {
     [SerializeField] private string itemName;
-    [SerializeField] private Sprite itemIcon;
-
     public string ItemName => itemName;
     private Color originalColor;
-    //
-    private SpriteRenderer spriteRenderer;
-    //
 
 
 
     private void Start()
     {
-        //
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = itemIcon;
-        //
         gameObject.AddComponent<MouseHover>();
         originalColor = GetComponent<SpriteRenderer>().color;
     }
@@ -44,7 +35,7 @@ public class Item : MonoBehaviour, IInteractable, IPointerClickHandler
 
     private void OnMouseUp()
     {
-        if (!DragScroller.IsDragging)
+        if(!DragScroller.IsDragging)
             Interact();
     }
 
@@ -61,10 +52,4 @@ public class Item : MonoBehaviour, IInteractable, IPointerClickHandler
     {
         GetComponent<SpriteRenderer>().color = originalColor;
     }
-    //
-    public Sprite GetItemIcon()
-    {
-        return itemIcon;
-    }
-    //
 }
