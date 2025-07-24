f6342ab919a05bf0ec5478125bdf46fcdef59029
kingdoza <kdowon2k98@naver.com>
2025-07-24 21:28:06 +0900

    CollectibleItem -> 인벤토리용 스프라이트 추가

--- Assets/Scripts/Interactable/CollectibleItem.cs
+++ Assets/Scripts/Interactable/CollectibleItem.cs
@@ -3,6 +3,10 @@ using UnityEngine.EventSystems;
 
 public class CollectibleItem : Item
 {
+    [SerializeField] private Sprite inventorySprite;
+
+
+
     public override void Interact()
     {
         //webhook test
