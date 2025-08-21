4bf5a30dc2f14bff0978668e490d01368d4a362c
kingdoza <kdowon2k98@naver.com>
2025-08-22 04:54:48 +0900

    해상도 비율 고정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -80,6 +80,18 @@ public class GameManager : PersistentSingleton<GameManager>
             }
         }
     }
+
+
+
+    public void SetResolution()
+    {
+        int setWidth = 1920; // 화면 너비
+        int setHeight = 1080; // 화면 높이
+
+        //해상도를 설정값에 따라 변경
+        //3번째 파라미터는 풀스크린 모드를 설정 > true : 풀스크린, false : 창모드
+        Screen.SetResolution(setWidth, setHeight, true);
+    }
     
 
 
@@ -87,6 +99,7 @@ public class GameManager : PersistentSingleton<GameManager>
     protected override void Awake()
     {
         base.Awake();
+        SetResolution();
         soundController = GetComponentInChildren<SoundController>();
 
         if (Instance == this)
